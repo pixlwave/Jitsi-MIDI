1487b504cfe25712c92df43f4f5e372a58779988
Doug <git@pixlwave.uk>
2021-07-11 21:44:26 +0100

    Fix octave numbers 🙄

--- Sources/NotesGrid.swift
+++ Sources/NotesGrid.swift
@@ -2,25 +2,25 @@ import SwiftUI
 
 struct NotesGrid: View {
     let notes = [
-        NoteModel(color: .green, text: "Toggle Mic", note: "C3"),
-        NoteModel(color: .red, text: "Toggle Camera", note: "C♯3"),
-        NoteModel(color: .blue, text: "Share Screen", note: "D3"),
-        NoteModel(color: .white, text: "Raise Hand", note: "E♭3"),
+        NoteModel(color: .green, text: "Toggle Mic", note: "C2"),
+        NoteModel(color: .red, text: "Toggle Camera", note: "C♯2"),
+        NoteModel(color: .blue, text: "Share Screen", note: "D2"),
+        NoteModel(color: .white, text: "Raise Hand", note: "E♭2"),
         
-        NoteModel(color: .pink, text: "Focus Me", note: "A♭2"),
-        NoteModel(color: .pink, text: "Focus #1", note: "A2"),
-        NoteModel(color: .pink, text: "Focus #2", note: "B♭2"),
-        NoteModel(color: .yellow, text: "Toggle Thumbnails", note: "B2"),
+        NoteModel(color: .pink, text: "Focus Me", note: "A♭1"),
+        NoteModel(color: .pink, text: "Focus #1", note: "A1"),
+        NoteModel(color: .pink, text: "Focus #2", note: "B♭1"),
+        NoteModel(color: .yellow, text: "Toggle Thumbnails", note: "B1"),
         
-        NoteModel(color: .pink, text: "Focus #3", note: "E2"),
-        NoteModel(color: .pink, text: "Focus #4", note: "F2"),
-        NoteModel(color: .pink, text: "Focus #5", note: "F♯2"),
-        NoteModel(color: .yellow, text: "Toggle Tile View", note: "G2"),
+        NoteModel(color: .pink, text: "Focus #3", note: "E1"),
+        NoteModel(color: .pink, text: "Focus #4", note: "F1"),
+        NoteModel(color: .pink, text: "Focus #5", note: "F♯1"),
+        NoteModel(color: .yellow, text: "Toggle Tile View", note: "G1"),
         
-        NoteModel(color: .orange, text: "Push To Talk", note: "C2"),
-        NoteModel(color: Color(.systemTeal), text: "Call Quality", note: "C♯2"),
-        NoteModel(color: Color(.systemTeal), text: "Speaker Stats", note: "D2"),
-        NoteModel(color: .blue, text: "Full Screen", note: "E♭2")
+        NoteModel(color: .orange, text: "Push To Talk", note: "C1"),
+        NoteModel(color: Color(.systemTeal), text: "Call Quality", note: "C♯1"),
+        NoteModel(color: Color(.systemTeal), text: "Speaker Stats", note: "D1"),
+        NoteModel(color: .blue, text: "Full Screen", note: "E♭1")
     ]
     
     var body: some View {
